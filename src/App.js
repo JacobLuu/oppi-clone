509f65aefbfedcbba014a6955939b64ed4001540
Lưu Bình Minh <luubinhminh1507@gmail.com>
2022-01-17 13:28:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [info, setInfo]= useState({
+    name:'Jacob',
+    age: 24,
+    address: 'Hà Nội'
+  });
+
+  const handleUpdate = () => {
+    setInfo(
+      pre => ({
+        ...pre,
+        name: 'Jacob Lưu',
+        bio: 'Hello'
+      }))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{padding:20}}>
+      <h1>{JSON.stringify(info)}</h1>
+      <button onClick = {handleUpdate}>Update</button>
+
     </div>
   );
 }
