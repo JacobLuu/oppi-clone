524dafcd66b1456ce83688a62c8c13d3d3a19c95
Lưu Bình Minh <luubinhminh1507@gmail.com>
2022-01-18 17:57:44 +0700

    create login

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import "./App.css";
-import React , { useState} from 'react';
-import Login from "./Components/LogIn/LogIn";
+import Login from "./Components/LogIn/Login";
 
 function App() {
-  const adminUser = {
-    email: "admin@admin.com",
-    password: "admin123"
-  }
-
-  const [user, setUser] = useState({name: "", email: ""});
-  const [error, setError] =  useState("");
-
-  const Login = details => {
-    console.log(details);
-  }
-  const Logout = () => {
-    console.log("Logout");
-  }
   return (
-      <div className="App">
-        {(user.email != "") ? (
-          <div className = "welcome">
-              <h2>Welcome, <span>{user.name}</span></h2>
-              <button>Logout</button>
-          </div>
-        ) : (
-          <Login />
-        )}
-      </div>
+    <div className="App">
+      <header> LOGIN </header>
+      <Login />
+    </div>
   );
 }
 
